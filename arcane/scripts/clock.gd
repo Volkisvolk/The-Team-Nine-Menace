d96c4e99c6945e8ebc48f67ca7fda756b74de277
Hannes Hofmeister <Hannes.hofmeister@uni-ulm.de>
2025-05-24 18:36:44 +0200

    Add Clock

---
+++ arcane/scripts/clock.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+@onready var day_hand: Sprite2D = $ClockHand
+@onready var day_label: Label = $DateLabel
+@onready var update_timer: Timer = $UpdateTimer
+@onready var day_timer: Timer = $DayTimer
+
+var time_passed := 0.0
+var current_day := 1
+
+func _ready():
+	update_timer.wait_time = 0.1
+	update_timer.timeout.connect(_on_update_timer_tick)
+	update_timer.start()
+
+	day_timer.wait_time = 60.0
+	day_timer.timeout.connect(_on_day_timer_timeout)
+	day_timer.start()
+
+	_update_ui()
+
+func _on_update_timer_tick():
+	time_passed += update_timer.wait_time
+	if time_passed > 60.0:
+		time_passed = 0.0  # oder hier auf 0 setzen, falls du keinen Drift willst
+	_update_ui()
+
+func _on_day_timer_timeout():
+	current_day += 1
+	time_passed = 0.0  # resette sicherheitshalber
+	_update_ui()
+
+func _update_ui():
+	day_hand.rotation_degrees = time_passed * 6.0 
+	day_label.text = "Tag %d" % current_day

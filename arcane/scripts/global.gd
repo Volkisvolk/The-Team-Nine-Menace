813fa01701b460f930e508358677e42fd34e92dd
= <=>
2025-05-24 13:24:41 +0200

    added resource variables

--- arcane/scripts/global.gd
+++ arcane/scripts/global.gd
@@ -1,1 +1,29 @@
 extends Node
+var gold
+var organic
+var food
+var chemical
+var drug
+var overworld_people
+var underworld_people
+
+func add_gold(val):
+	gold += val
+	
+func add_organic(val):
+	organic += val
+	
+func add_food(val):
+	food += val
+	
+func add_chemical(val):
+	chemical += val
+	
+func add_drug(val):
+	drug += val
+	
+func add_overworld_people(val):
+	overworld_people += val
+	
+func add_underworld_people(val):
+	underworld_people += val
